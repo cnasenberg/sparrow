16abfbdddfdf368f0d74610a66be7809ce1b2419
Claudia Nasenberg <claudia@nasenberg.eu>
2015-07-11 23:22:35 +0200

    Added @Service annotations to service implementations. Corrected redundant use of this.

--- src/main/java/klingcase/GmailService.java
+++ src/main/java/klingcase/GmailService.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class GmailService implements EmailService {
 
-      public void sendEmail(String emailAddress, String subject, String body) {
+	  public void sendEmail(String emailAddress, String subject, String body) {
 		System.out.println("... Sending email to "+emailAddress+"... ");
 		// send email via gmail account  
-      }
+	  }
 
 }
