4dc600fdc22754cd815a0ea0a08197f3dc863c59
Claudia Nasenberg <claudia@nasenberg.eu>
2015-07-12 21:48:09 +0200

    Made Application compile and run with just one statement. Added this statement to the new README. Application now reads name and email address from console. Removed duplicate beans from Application.

--- src/main/java/klingcase/Application.java
+++ src/main/java/klingcase/Application.java
@@ -2,31 +2,36 @@ package klingcase;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+import java.io.Console;
+import java.io.IOException;
 
 @Configuration
 @ComponentScan
 public class Application {
 
-	@Bean
     UserService userService() {
 		// create depending on configuration
 		return new UserServiceImpl();
 	}
 	
-	@Bean
     EmailService emailService() { 
 		// create depending on configuration
 		return new GmailService();
     }
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 		UserService userService = context.getBean(UserService.class);
-		EmailService emailService = context.getBean(EmailService.class);
+		
+		Console console = System.console();
+        if (console == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
 
-		String name = args[0];
-		String emailAddress = args[1];
+        String name = console.readLine("Please enter your name: ");
+        String emailAddress = console.readLine("Please enter your email address: ");
 		userService.createUser(name, emailAddress);
 	}
 }
