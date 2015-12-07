6f3f80bb0f7ef58eaa53e84358cc8f6d782077cd
Claudia <claudia@nasenberg.eu>
2015-12-07 23:54:50 +0100

    Added logging, configured to write to file sparrow.log. Added a missing dependency for the code coverage plugin.

--- src/main/java/sparrow/Application.java
+++ src/main/java/sparrow/Application.java
@@ -3,13 +3,17 @@ package sparrow;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Console;
 import java.io.IOException;
 
 public class Application {
+    
+    final static Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws IOException {
-
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppContext.class);
 		
 		Console console = System.console();
@@ -17,6 +21,8 @@ public class Application {
             System.err.println("No console.");
             System.exit(1);
         }
+        
+        logger.info("Entering application.");
 
         String name = console.readLine("Please enter your name: ");
         String emailAddress = console.readLine("Please enter your email address: ");
@@ -24,5 +30,6 @@ public class Application {
         UserService userService = context.getBean(UserService.class);
 		userService.createUser(name, emailAddress);		
 		
+        logger.info("Exiting application.");
 	}
 }
